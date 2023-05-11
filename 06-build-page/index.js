0f5c336c7a759bb774a1619555ed60592c6fb6b0
Maksym Voloshyn <mv.webdew@gmail>
2023-05-11 15:20:52 +0300

    refactor: rewrite a solution to the build-page to Promises

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,179 +1,107 @@
-const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
-function createProjectBuild() {
+createProjectDir()
+  .then(() => copyAssets())
+  .then(() => createBundleCSS())
+  .then(() => createBundleHTML())
+  .catch((error) => console.log(error));
+
+async function createProjectDir() {
   const dist = path.join(__dirname, 'project-dist');
 
-  fs.access(dist, (error) => {
-    if (error) {
-      createProjectDir(dist);
-    } else {
-      fs.rm(dist, { recursive: true }, (error) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        createProjectDir(dist);
-      });
-    }
-  });
+  if (await isHasDirectory(dist)) {
+    await fsPromises.rm(dist, { recursive: true });
+  }
+
+  await fsPromises.mkdir(dist, { recursive: true });
 }
 
-function createProjectDir(dist) {
-  fs.mkdir(dist, (error) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
+async function isHasDirectory(dist) {
+  try {
+    await fsPromises.access(dist);
+  } catch (error) {
+    return false;
+  }
 
-    const pathToAssets = path.join(__dirname, 'assets');
-    const pathToCopy = path.join(dist, 'assets');
-
-    copyAssets(pathToAssets, pathToCopy);
-    createBundleCSS();
-    createBundleHTML();
-  });
+  return true;
 }
 
-function copyAssets(src, dist) {
-  fs.mkdir(dist, (error) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
+async function copyAssets(...args) {
+  let src = path.join(__dirname, 'assets');
+  let dist = path.join(__dirname, 'project-dist', 'assets');
 
-    fs.readdir(src, (error, files) => {
-      if (error) {
-        console.error(error);
-        return;
+  if (args.length) {
+    src = args[0];
+    dist = args[1];
+  }
+
+  try {
+    await fsPromises.mkdir(dist, { recursive: true });
+    const files = await fsPromises.readdir(src);
+
+    for (const file of files) {
+      const pathToFile = path.join(src, file);
+      const pathToCopy = path.join(dist, file);
+      const stats = await fsPromises.stat(pathToFile);
+
+      if (stats.isFile()) {
+        await fsPromises.copyFile(pathToFile, pathToCopy);
+      } else if (stats.isDirectory()) {
+        await copyAssets(pathToFile, pathToCopy);
       }
-
-      files.forEach((file) => {
-        const pathToFile = path.join(src, file);
-
-        fs.stat(pathToFile, (error, stats) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
-
-          const pathToCopy = path.join(dist, file);
-
-          if (stats.isFile()) {
-            fs.copyFile(pathToFile, pathToCopy, 0, (error) => {
-              if (error) {
-                console.error(error);
-                return;
-              }
-            });
-          } else if (stats.isDirectory()) {
-            return copyAssets(pathToFile, pathToCopy);
-          }
-        });
-      });
-    });
-  });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function createBundleCSS() {
+async function createBundleCSS() {
   const src = path.join(__dirname, 'styles');
   const bundle = path.join(__dirname, 'project-dist', 'style.css');
 
-  fs.readdir(src, (error, files) => {
-    if (error) {
-      console.error(error);
-      return;
+  try {
+    await fsPromises.writeFile(bundle, '');
+    const files = await fsPromises.readdir(src);
+
+    for (const file of files) {
+      const pathToFile = path.join(src, file);
+      const stats = await fsPromises.stat(pathToFile);
+
+      if (stats.isFile() && path.extname(pathToFile) === '.css') {
+        const temp = await fsPromises.readFile(pathToFile, 'utf8');
+        await fsPromises.appendFile(bundle, temp);
+      }
     }
-
-    fs.writeFile(bundle, '', (error) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-    });
-
-    files.forEach((file) => {
-      const fullPath = path.join(src, file);
-
-      fs.stat(fullPath, (error, stats) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-
-        if (stats.isFile() && path.extname(fullPath) === '.css') {
-          appendFile(fullPath, bundle);
-        }
-      });
-    });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function appendFile(src, dist) {
-  const stream = fs.createReadStream(src, 'utf8');
-
-  stream.on('data', (chunk) => {
-    fs.appendFile(dist, chunk, (error) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-    });
-  });
-}
-
-function createBundleHTML() {
+async function createBundleHTML() {
   const src = path.join(__dirname, 'template.html');
   const bundle = path.join(__dirname, 'project-dist', 'index.html');
   const componentsPath = path.join(__dirname, 'components');
-  let template = '';
 
-  fs.readFile(src, 'utf-8', (error, data) => {
-    if (error) {
-      console.log(error);
+  try {
+    let template = await fsPromises.readFile(src, 'utf8');
+    const components = await fsPromises.readdir(componentsPath);
+    await fsPromises.writeFile(bundle, '');
+
+    for (const component of components) {
+      const pathToFile = path.join(componentsPath, component);
+      const stats = await fsPromises.stat(pathToFile);
+
+      if (stats.isFile() && path.extname(pathToFile) === '.html') {
+        const componentName = path.basename(pathToFile).slice(0, -5);
+        const regex = new RegExp(`{{\\s*${componentName}\\s*}}`, 'g');
+
+        const component = await fsPromises.readFile(pathToFile, 'utf8');
+        template = template.replace(regex, component);
+        await fsPromises.writeFile(bundle, template);
+      }
     }
-    template = data;
-
-    fs.writeFile(bundle, '', (error) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-    });
-
-    fs.readdir(componentsPath, (error, files) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      files.forEach((file) => {
-        const fullPath = path.join(componentsPath, file);
-
-        fs.stat(fullPath, (error, stats) => {
-          if (error) {
-            console.error(error);
-            return;
-          }
-
-          if (stats.isFile() && path.extname(fullPath) === '.html') {
-            const component = path.basename(fullPath).slice(0, -5);
-            const regex = new RegExp(`{{\\s*${component}\\s*}}`, 'g');
-
-            fs.readFile(fullPath, 'utf8', (error, data) => {
-              template = template.replace(regex, data);
-
-              fs.writeFile(bundle, template, (error) => {
-                if (error) {
-                  console.error(error);
-                  return;
-                }
-              });
-            });
-          }
-        });
-      });
-    });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 }
-createProjectBuild();
